cf34f844efb94c1b924a3c5deb3ac80aa81aafa8
APP-User2 <lixb@feimarobotics.com>
2016-10-09 11:43:18 +0800

    Initial commit

---
+++ app/src/main/java/com/feima/timerandthreadsleep/MainActivity.java
@@ -0,0 +1,88 @@
+package com.feima.timerandthreadsleep;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    private final String TAG = "test";
+
+    private EditText mEtFrequency;
+    private Timer mTimer;
+    private boolean running;
+    private int mPeriod;
+    private Handler mHandler = new Handler();
+    private Runnable mRunnable;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mEtFrequency = (EditText) findViewById(R.id.et_frequency);
+    }
+
+    public void timerSend(View view) {
+        int freq = Integer.parseInt(mEtFrequency.getText().toString());
+        mPeriod = 1000 / freq;
+        mTimer = new Timer();
+        TimerTask task = new TimerTask() {
+            @Override
+            public void run() {
+                Log.e(TAG, "timer send");
+            }
+        };
+        mTimer.schedule(task, 0, mPeriod);
+    }
+
+    public void threadSend(View view) {
+        int freq = Integer.parseInt(mEtFrequency.getText().toString());
+        mPeriod = 1000 / freq;
+        running = true;
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                while (true) {
+                    SystemClock.sleep(mPeriod);
+                    if (running) {
+                        Log.e(TAG, "thread send");
+                    }
+                }
+            }
+        };
+        thread.start();
+    }
+
+    public void handlerSend(View view) {
+        int freq = Integer.parseInt(mEtFrequency.getText().toString());
+        mPeriod = 1000 / freq;
+
+        mRunnable = new Runnable(){
+            @Override
+            public void run() {
+                Log.e(TAG, "handler send");
+                mHandler.postDelayed(mRunnable, mPeriod);
+            }
+        };
+
+    }
+
+    public void stopSend(View view) {
+        if (null!=mTimer) {
+            mTimer.cancel();
+        }
+
+        running = false;
+
+        mHandler.removeCallbacks(mRunnable);
+
+    }
+}
